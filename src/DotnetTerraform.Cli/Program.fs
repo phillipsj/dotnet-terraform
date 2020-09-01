7c843e39ae45b1cc6f2c4898a32d337520bd0aa3
Jamie Phillips <cphill11@gmail.com>
2020-08-31 23:47:11 -0400

    Adding some basics to get started.

---
+++ src/DotnetTerraform.Cli/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Argu
+
+type CliArguments =
+    | Install
+    | Fmt of args: string
+    | Apply of args: string
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
