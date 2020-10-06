f12e81eda2e45f13ef96724a5554821c02253c6e
Jamie Phillips <cphill11@gmail.com>
2020-10-05 20:51:52 -0400

    Adding nuget push to Fake script.

--- build.fsx
+++ build.fsx
@@ -2,6 +2,7 @@
 nuget Fake.DotNet.Cli
 nuget Fake.IO.FileSystem
 nuget Fake.Core.Target //"
+
 #load ".fake/build.fsx/intellisense.fsx"
 open Fake.Core
 open Fake.DotNet
@@ -12,21 +13,28 @@ open Fake.Core.TargetOperators
 
 Target.initEnvironment ()
 
-Target.create "Clean" (fun _ ->
-    !! "src/**/bin"
-    ++ "src/**/obj"
-    |> Shell.cleanDirs 
-)
+Target.create "Clean" (fun _ -> !! "src/**/bin" ++ "src/**/obj" |> Shell.cleanDirs)
 
-Target.create "Build" (fun _ ->
-    !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build id)
-)
+Target.create "Build" (fun _ -> !! "src/**/*.*proj" |> Seq.iter (DotNet.build id))
+
+Target.create "Push" (fun _ ->
+    let apiKey =
+        Environment.environVarOrFail "NUGET_API_KEY"
+
+    let setNugetPushParams (defaults: NuGet.NuGet.NuGetPushParams) =
+        { defaults with
+              DisableBuffering = true
+              ApiKey = Some apiKey }
+
+    let setParams (defaults: DotNet.NuGetPushOptions) =
+        { defaults with
+              PushParams = setNugetPushParams defaults.PushParams }
+
+    !! "src/**/nupkg/*.nupkg"
+    |> Seq.iter (fun nupkg -> DotNet.nugetPush setParams nupkg))
 
 Target.create "All" ignore
 
-"Clean"
-  ==> "Build"
-  ==> "All"
+"Clean" ==> "Build" ==> "All"
 
 Target.runOrDefault "All"
